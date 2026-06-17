4745062c89becade32570e2d18da702310cefdc5
Davirain <davirain.yin@gmail.com>
2026-06-17 11:06:32 +0800

    test: cover String replace boundaries in EmitZig

--- tests/emitzig/StdlibStringReplace.lean
+++ tests/emitzig/StdlibStringReplace.lean
@@ -13,3 +13,12 @@ def main : IO Unit := do
   IO.println ("red green blue".replace "e" "E")
   IO.println ("aaaaa".replace "aa" "b")
   IO.println ("abc".replace "" "k")
+  IO.println ("abababa".replace "aba" "X")
+  IO.println ("abc".replace "z" "Z")
+  IO.println ("hé hé".replace 'é' "e")
+  IO.println ("hé hé".replace "hé".toSlice "LE")
+  let replacement := "XYZ".toSlice.take 2
+  IO.println ("banana".replace "na" replacement)
+  let slice := "red green blue".toSlice.drop 4
+  IO.println (slice.replace "e" "E")
+  IO.println ("".replace "" "x")
