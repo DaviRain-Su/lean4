f130d6253518712e1105815be37e2b817ceb4e23
Davirain <davirain.yin@gmail.com>
2026-06-17 09:16:46 +0800

    test: cover HTTP URI builder variants in EmitZig

--- tests/emitzig/StdlibHttpURI.lean
+++ tests/emitzig/StdlibHttpURI.lean
@@ -7,6 +7,38 @@ import Std.Http.Data.URI.Basic
 
 open Std.Http
 
+def printBuilderVariants (ipv4 : Std.Net.IPv4Addr) (ipv6 : Std.Net.IPv6Addr) : IO Unit := do
+  let withAuthority :=
+    URI.Builder.empty
+      |>.setScheme! "HTTP"
+      |>.setUserInfo "name space" (some "p@ss")
+      |>.setHostIPv4 ipv4
+      |>.setPort 8080
+      |>.setPath #["api", "v1"]
+      |>.setQuery #[("x y", some "1+2"), ("debug", none)]
+      |>.setFragment "frag/value"
+      |>.build
+  IO.println (toString withAuthority)
+  IO.println (withAuthority.authority.map toString |>.getD "none")
+  IO.println (withAuthority.fragment.getD "none")
+
+  let localPath : URI.Path := { segments := #[URI.EncodedSegment.encode "local"], absolute := true }
+  let withoutAuthority :=
+    withAuthority
+      |>.withScheme! "https"
+      |>.withAuthority none
+      |>.withPath localPath
+      |>.withQuery URI.Query.empty
+      |>.withFragment none
+  IO.println (toString withoutAuthority)
+
+  let withIPv6 :=
+    URI.Builder.empty
+      |>.setHostIPv6 ipv6
+      |>.setPath #["v6"]
+      |>.build
+  IO.println (toString withIPv6)
+
 def main : IO Unit := do
   let ipv4 := (Std.Net.IPv4Addr.ofString "127.0.0.1").getD (Std.Net.IPv4Addr.ofParts 127 0 0 1)
   let ipv6 := (Std.Net.IPv6Addr.ofString "2001:db8::1").getD (Std.Net.IPv6Addr.ofParts 0x2001 0x0db8 0 0 0 0 0 1)
@@ -37,3 +69,4 @@ def main : IO Unit := do
   IO.println target.query.toRawString
   IO.println (toString ipv4)
   IO.println (toString ipv6)
+  printBuilderVariants ipv4 ipv6
