a4af53a898016b0fe45af54c5bdee0ce3165c235
Davirain <davirain.yin@gmail.com>
2026-06-17 11:39:18 +0800

    test: cover iterator combinators in EmitZig

--- tests/emitzig/StdlibIterators.lean
+++ tests/emitzig/StdlibIterators.lean
@@ -4,6 +4,8 @@ import Std.Data.Iterators
 import Std.Data.Iterators.Consumers.Set
 import Init.Data.Iterators.Combinators
 import Init.Data.Array.GetLit
+import Init.Data.Hashable
+import Init.Data.Range.Polymorphic
 import Init.System.IO
 
 /-! End-to-end stdlib-linked EmitZig test for `Std.Data.Iterators` producers, combinators, and consumers. -/
@@ -33,3 +35,30 @@ def main : IO Unit := do
   IO.println tree.size
   IO.println (tree.contains 4)
   IO.println (tree.contains 2)
+  let taken := xs.iter.take 3 |>.toArray
+  IO.println taken.size
+  IO.println taken[2]!
+  let appended := (#[1, 2].iter.append #[8, 9, 10].iter).toList
+  IO.println appended.length
+  IO.println (appended.foldl (fun acc n => acc + n) 0)
+  let flattened := #[1, 3, 5].iter.flatMap (fun n => #[n, n + 1].iter) |>.toArray
+  IO.println flattened.size
+  IO.println flattened[5]!
+  let revList := xs.iter.drop 3 |>.toListRev
+  IO.println revList.length
+  IO.println (revList.foldl (fun acc n => acc * 10 + n) 0)
+  let accessRange := (0...=8 : Std.Rcc Nat).iter
+  match accessRange.atIdx? 4 with
+  | some n => IO.println n
+  | none => IO.println "none"
+  IO.println (accessRange.atIdx? 9).isNone
+  let repeated := (Std.Iter.repeat (fun n => n + 3) 1).take 4 |>.toArray
+  IO.println repeated.size
+  IO.println repeated[3]!
+  let stepped := ((0...=10 : Std.Rcc Nat).iter.stepSize 3).toArray
+  IO.println stepped.size
+  IO.println stepped[3]!
+  let hashSet := xs.iter.filter (fun n => n ≤ 3) |>.toHashSet
+  IO.println hashSet.size
+  IO.println (hashSet.contains 2)
+  IO.println (hashSet.contains 5)
