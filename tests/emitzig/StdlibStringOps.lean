cb9961f5b7d3ddea5479c02dc2f8b29d52798a7b
Davirain <davirain.yin@gmail.com>
2026-06-17 10:53:16 +0800

    test: cover String UTF-8 boundaries in EmitZig

--- tests/emitzig/StdlibStringOps.lean
+++ tests/emitzig/StdlibStringOps.lean
@@ -3,9 +3,21 @@ prelude
 import Init.Data.String.Basic
 import Init.Data.String.Bootstrap
 import Init.Data.String.Length
+import Init.Data.String.Search
+import Init.Data.Array.GetLit
 import Init.System.IO
 
 /-! End-to-end stdlib-linked EmitZig test for String push, append, length, and ordering. -/
+
+def printChar? (c : Option Char) : IO Unit := do
+  match c with
+  | some c => IO.println c
+  | none => IO.println "none"
+
+def printString? (s : Option String) : IO Unit := do
+  match s with
+  | some s => IO.println s
+  | none => IO.println "none"
 
 def main : IO Unit := do
   let mut s := "h"
@@ -13,7 +25,25 @@ def main : IO Unit := do
   s := s ++ "llo"
   IO.println s
   IO.println s.length
+  IO.println s.utf8ByteSize
+  IO.println s.toUTF8.size
+  IO.println "".isEmpty
+  IO.println s.isEmpty
+  printChar? s.front?
+  printChar? s.back?
   if "hé" < s then
     IO.println "less"
   else
     IO.println "not less"
+  let u := "L∃∀N"
+  IO.println u.length
+  IO.println u.utf8ByteSize
+  match u.pos? ⟨1⟩ with
+  | some p => printChar? p.get?
+  | none => IO.println "none"
+  IO.println (u.pos? ⟨2⟩).isSome
+  match u.pos? ⟨4⟩ with
+  | some p => printChar? p.get?
+  | none => IO.println "none"
+  printString? (String.fromUTF8? s.toUTF8)
+  printString? (String.fromUTF8? (ByteArray.mk #[255]))
