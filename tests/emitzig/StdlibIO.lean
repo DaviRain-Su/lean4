e12951b5d1048119a21f3a608102638a263234d4
Davirain <davirain.yin@gmail.com>
2026-06-17 10:07:45 +0800

    test: cover file handle IO in EmitZig

--- tests/emitzig/StdlibIO.lean
+++ tests/emitzig/StdlibIO.lean
@@ -7,8 +7,19 @@ import Init.System.IO
 
 def main : IO Unit := do
   IO.FS.withTempFile fun h _path => do
-    h.putStr "zig-io"
+    h.putStrLn "zig-io"
+    h.putStr "tail"
     h.flush
     h.rewind
-    let s ← h.readToEnd
-    IO.println s.length
+    let line ← h.getLine
+    IO.println line.length
+    let rest ← h.readToEnd
+    IO.println rest.length
+    h.rewind
+    let bytes ← h.read 3
+    IO.println bytes.size
+    IO.println (bytes.get! 0).toNat
+    h.truncate
+    h.rewind
+    let truncated ← h.readToEnd
+    IO.println truncated.length
