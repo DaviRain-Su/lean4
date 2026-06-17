73be5662b21da88ef867d7b9270792066c1d58ca
Davirain <davirain.yin@gmail.com>
2026-06-17 10:05:50 +0800

    test: cover big Nat division in EmitZig

--- tests/emitzig/StdlibNatBig.lean
+++ tests/emitzig/StdlibNatBig.lean
@@ -9,3 +9,10 @@ def main : IO Unit := do
   let n := (2 : Nat) ^ 100
   let m := n * 3 + 1
   IO.println m
+  IO.println (m / n)
+  IO.println (m % n)
+  IO.println (m - n)
+  let dec := (10 : Nat) ^ 50 + 12345
+  IO.println dec
+  IO.println (dec / 97)
+  IO.println (dec % 97)
